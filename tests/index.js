159a2706393d014bc1c7cb593c23979bdb9ae43b
tester_pivka17 <github@svdgod.ru>
2024-01-30 15:13:28 +0500

    f

---
+++ tests/index.js
@@ -0,0 +1,25 @@
+
+const path = require ('path');
+const osu_tools = require('osu-tools');
+
+const result1 = osu_tools.scores_db_load('data\\1\\scores.db', osu_tools.all_score_properties);
+const result2 = osu_tools.scores_db_load('data\\2\\scores.db', osu_tools.all_score_properties);
+const result3 = osu_tools.scores_db_load('data\\3\\scores.db', osu_tools.all_score_properties);
+
+/*
+osu_tools.scores_db_save(result1, 'data\\1\\scores_2.db');
+osu_tools.scores_db_save(result2, 'data\\2\\scores_2.db');
+osu_tools.scores_db_save(result3, 'data\\3\\scores_2.db');
+
+console.log('scores.db 1', osu_tools.compare_files('data\\1\\scores.db', 'data\\1\\scores_2.db'));
+console.log('scores.db 2', osu_tools.compare_files('data\\2\\scores.db', 'data\\2\\scores_2.db'));
+console.log('scores.db 3', osu_tools.compare_files('data\\3\\scores.db', 'data\\3\\scores_2.db'));*/
+
+let res = osu_tools.scores_db_concat(result1, result2)
+res = osu_tools.scores_db_concat(res, result3)
+
+osu_tools.scores_db_save(res, 'scores.db');
+
+const result5 = osu_tools.scores_db_load(path.join('scores.db'), osu_tools.all_score_properties);
+osu_tools.scores_db_save(result5, 'scores_n2.db');
+
